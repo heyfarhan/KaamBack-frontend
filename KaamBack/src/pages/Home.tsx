ff3d56af33bc1a9fd09fdcb2efb7be3155814263
Aryaman3007 <aryaman3007@gmail.com>
2024-07-11 17:08:04 +0530

    about us font

--- KaamBack/src/pages/Home.tsx
+++ KaamBack/src/pages/Home.tsx
@@ -65,11 +65,11 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="bg-white px-6 lg:px-24 py-12">
+        <div className="bg-white px-6 lg:px-24 lg:py-12 py-6">
           <h1 className="text-3xl font-bold text-center lg:text-left text-[#181B38]">About Us</h1>
-          <div className='flex lg:flex-row mt-[50px] flex-col'>
+          <div className='flex lg:flex-row lg:mt-[50px] mt-[30px] flex-col'>
             <div className='lg:w-1/2 w-full'>
-              <p className='font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas diam in arcu cursus euismod quis. Sed nisi lacus sed viverra tellus. Imperdiet proin fermentum leo vel orci porta non pulvinar neque. Tellus orci ac auctor augue mauris augue. Vitae tortor condimentum lacinia quis vel eros donec ac odio. Aliquam malesuada bibendum arcu vitae elementum. Nunc mattis enim ut tellus elementum sagittis vitae. Orci porta non pulvinar neque laoreet. Montes nascetur ridiculus mus mauris vitae ultricies. Laoreet id donec ultrices tincidunt arcu non sodales. Gravida arcu ac tortor dignissim convallis.
+              <p className='lg:font-medium font-normal'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas diam in arcu cursus euismod quis. Sed nisi lacus sed viverra tellus. Imperdiet proin fermentum leo vel orci porta non pulvinar neque. Tellus orci ac auctor augue mauris augue. Vitae tortor condimentum lacinia quis vel eros donec ac odio. Aliquam malesuada bibendum arcu vitae elementum. Nunc mattis enim ut tellus elementum sagittis vitae. Orci porta non pulvinar neque laoreet. Montes nascetur ridiculus mus mauris vitae ultricies. Laoreet id donec ultrices tincidunt arcu non sodales. Gravida arcu ac tortor dignissim convallis.
                 <br></br><br></br>
                 Nibh sed pulvinar proin gravida hendrerit lectus a. Et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Orci sagittis eu volutpat odio facilisis mauris sit. Purus in mollis nunc sed. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Tempus imperdiet nulla malesuada pellentesque elit eget gravida. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor. Pellentesque dignissim enim sit amet venenatis urna cursus eget. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare. Id leo in vitae turpis massa sed. Sagittis eu volutpat odio facilisis mauris sit.</p>
             </div>
